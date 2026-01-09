8ecfe22155522dff2e0083e9250b744884045ae8
cakradana <rcakradana@gmail.com>
2026-01-09 11:10:08 +0700

    1.0.4

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import MainLayout from "@/layouts/MainLayout";
 
 export default function Home() {
-  console.log("FE Version: 1.0.3");
+  console.log("FE Version: 1.0.4");
   return (
     <MainLayout>
       <div className="flex min-h-full items-center justify-center">
