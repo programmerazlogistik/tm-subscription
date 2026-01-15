6a7763d966629bd69ed0331ed6410e51d3386907
cakradana <rcakradana@gmail.com>
2026-01-15 12:06:59 +0700

    fix suspense

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Suspense } from "react";
+
 import { TranslationProvider } from "@/hooks/use-translation";
 
 export function Providers({ children }) {
   console.log("FE Version: 1.0.17");
 
-  return <TranslationProvider>{children}</TranslationProvider>;
+  return (
+    <Suspense fallback={null}>
+      <TranslationProvider>{children}</TranslationProvider>
+    </Suspense>
+  );
 }
